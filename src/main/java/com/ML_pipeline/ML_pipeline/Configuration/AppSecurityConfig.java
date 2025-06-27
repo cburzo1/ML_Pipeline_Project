ea8e2923e0e27fcb3ffd1e0f9b50de8e561c35ff
cburzo1 <gm.c.burzo@gmail.com>
2025-06-26 20:01:48 -0400

    r_com

--- src/main/java/com/ML_pipeline/ML_pipeline/Configuration/AppSecurityConfig.java
+++ src/main/java/com/ML_pipeline/ML_pipeline/Configuration/AppSecurityConfig.java
@@ -1,14 +1,25 @@
 package com.ML_pipeline.ML_pipeline.Configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfig {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -18,10 +29,29 @@ public class AppSecurityConfig {
                 )
                 .csrf(csrf -> csrf.disable())
                 .httpBasic(httpBasic -> httpBasic.disable())
-                .formLogin(form -> form.disable());
+                .formLogin(Customizer.withDefaults());//form -> form.disable()
 
         System.out.println("🔐 Security config applied");
 
         return http.build();
+    }
+
+ //   @Bean
+ //   public UserDetailsService userDetailsService(){
+
+//        UserDetails user1 = User
+//                .withDefaultPasswordEncoder()
+//                .username("")
+
+   //     return new InMemoryUserDetailsManager();
+    //}
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(NoOp);
+
+        return provider;
     }
 }
